9f118cf6fd7034046639fe011edfc14e8b6f24e2
CDUGUINE <cduguine@gmail.com>
2024-05-19 09:27:34 +0200

    Etape 3 (sans documentation technique). bddmanager, dal et model créés.

---
+++ gestionPersonnel/model/service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionPersonnel.model
+{
+    public class Service
+    {
+        public int Idservice { get; }
+        public string Nom { get; }
+
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name="idservice"></param>
+        /// <param name="nom"></param>
+        public Service(int idservice, string nom)
+        {
+            this.Idservice = idservice;
+            this.Nom = nom;
+        }
+
+        /// <summary>
+        /// Définit l'information à afficher (juste le nom)
+        /// </summary>
+        /// <returns>nom du profil</returns>
+        public override string ToString()
+        {
+            return this.Nom;
+        }
+    }
+}
